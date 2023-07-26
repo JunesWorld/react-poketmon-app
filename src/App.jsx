93ae8549970af4ce7b17280863f7acf4a74ea34a
June <sungj1207@gmail.com>
2023-07-26 23:48:20 +0900

    API를 통해 포켓몬 데이터 가져오기

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,23 @@ function App() {
       <header className='flex flex-col gap-2 w-full px-4 z-50'>
         Input Form
       </header>
+      <section className='pt-6 flex flex-col justify-content items-center overflow-auto z-0'>
+        <div className='flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl'>
+          {pokemons.length > 0 ? 
+          (
+            pokemons.map(({url, name}, index) => {
+              <div>
+                {name}
+              </div>
+            })
+          ) : 
+          (
+            <h2 className='font-medium text-lg text-slate-900 mb-1'>
+              포켓몬이 없습니다.
+            </h2>
+          )}
+        </div>
+      </section>
     </article>
   )
 }
