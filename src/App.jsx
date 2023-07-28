6ea15f58430b7632dc28dca278ac96af48964687
June <sungj1207@gmail.com>
2023-07-28 15:51:28 +0900

    검색기능 생성하기

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,9 @@ function App() {
   // 20개씩 보이고 더 보기 누르면 20개 추가
   const [offset, setOffset] = useState(0);
   const [limit, setLimit] = useState(20);
+
+  // Search 기능
+  const [searchTerm, setSerchTerm] = useState("");
   
 
   // 3. useEffect 호출
@@ -34,12 +37,45 @@ function App() {
         console.error(error);
     }
   }
+  
+  const handleSearchInput = async (e) => {
+    setSerchTerm(e.target.value);
+    if(e.target.value.length > 0) {
+      try {
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${e.target.value}`)
+        const pokemonData = {
+          url: `https://pokeapi.co/api/v2/pokemon/${response.data.id}`,
+          name: searchTerm
+        }
+        setPokemons([pokemonData])
+      } catch (error) {
+        setPokemons([]);
+        console.error(error);
+      }
+    } else {
+      fetchPokeData(true);
+    }
+  }
 
   // 2. Rendering
   return (
     <article className='pt-6'>
       <header className='flex flex-col gap-2 w-full px-4 z-50'>
-        Input Form
+        {/* Search UI */}
+        <div className='relative z-50'>
+          <form className='relative flex justify-center items-center w-[20.5rem] h-6 rounded-lg m-auto'>
+            <input 
+              type='text' 
+              value={searchTerm}
+              onChange={handleSearchInput}
+              className='text-xs w-[20.5rem] h-6 px-2 py-1 bg-[hsl(214,13%,47%)] rounded-lg text-gray-300 text-center' />
+            <button 
+              type='submit'
+              className='text-xs bg-slate-900 text-slate-300 w-[2.5rem] h-6 px-2 py-1 rounded-r-lg text-center absolute right-0 hover:bg-slate-700'>
+              검색
+            </button>
+          </form>
+        </div>
       </header>
       <section className='pt-6 flex flex-col justify-content items-center overflow-auto z-0'>
         <div className='flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl'>
